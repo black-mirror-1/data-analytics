9c74aaccf5a703c791173c80d96419c90bbaacc9
Sandeep Palavalasa <sandeep.palavalasa@gmail.com>
2019-02-21 13:51:08 -0800

    Week16 Homework

--- Week 16 Homework/assets/js/app.js
+++ Week 16 Homework/assets/js/app.js
@@ -17,35 +17,47 @@ var svg = d3
 var chartGroup = svg.append('g')
             .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-d3.csv('assets/data/data.csv', function(error,healthCameData) {
-    if (error) throw error;
+d3.csv('assets/data/data.csv').then(function(healthCareData) {
+    // if (error) throw error;
 
-    console.log(healthCameData);
-     healthCameData.forEach(state => {
-        state.healthcare =+ state.healthcare;
-        state.poverty =+ state.poverty;
-        // console.log(state.healthcare)
+    console.log(healthCareData);
+    healthCareData.forEach(state => {
+        console.log(state.healthcare)
+        state.healthcare = parseFloat(state.healthcare);
+        state.poverty = parseFloat(state.poverty);
      });
 
     // setup x 
-    var xScale = d3.scaleLinear().domain(d3.extent(healthcare, d => d.healthcare)).range([0, width]);
+    var xScale = d3.scaleLinear()
+            .domain(d3.extent(healthCareData, d => d.healthcare))
+            .range([0, width]);
     var xAxis = d3.axisBottom(xScale);
     // setup y
-    var yScale = d3.scaleLinear().domain([0, d3.max(healthcare, d => d.poverty)]).range([height, 0]); // value -> display
+    var yScale = d3.scaleLinear()
+            .domain([0, d3.max(healthCareData, d => d.poverty)])    
+            .range([height, 0]); // value -> display
     var yAxis = d3.axisLeft(yScale);
     
     // Add x-axis
     chartGroup.append("g")
     .attr("transform", `translate(0, ${height})`)
-    .call(bottomAxis);
+    .call(xAxis);
 
     // Add y1-axis to the left side of the display
     chartGroup.append("g")
-        .call(leftAxis);
+        .call(yAxis);
 
-    chartGroup.selectAll('.circle')
-        .data(healthCameData)
+    chartGroup.selectAll('.stateCircle')
+        .data(healthCareData)
         .enter()
         .append('circle')
-        .classed('circle', true)
+        .classed('stateCircle', true)
+        .attr('cx',d => xScale(d.healthcare))
+        .attr('cy', d => height - yScale(d.poverty))
+        .attr('r',"10")
+        .append('text')
+        .classed('stateText', true)
+        .text(d => d.abbr)
+        .attr('x','50%')
+        .attr('y','50%')
 });
